63863b8e60bb3f8db11de6b0a802324e8c9e2765
NWalter74 <nicole.walter@iths.se>
2022-10-19 09:08:56 +0200

    Added emaillfield and made some stylings

--- WpfÖvning/MainWindow.xaml.cs
+++ WpfÖvning/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -42,6 +43,43 @@ namespace WpfÖvning
         private void TBox_WriteToFile_TextChanged(object sender, MouseButtonEventArgs e)
         {
             TBox_WriteToFile.Text = "";
+        }
+
+        private void TBox_SendMail_TextChanged(object sender, MouseButtonEventArgs e)
+        {
+            TBox_SendMail.Text = "";
+        }
+
+        private void B_SendMail_Click(object sender, RoutedEventArgs e)
+        {
+            //Create pattern for email
+            string pattern = @"^((\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)\s*[;]{0,1}\s*)+$";
+
+            //Create a Regex
+            Regex rg = new Regex(pattern);
+
+            Match match = rg.Match(TBox_SendMail.Text);
+
+            if (match.Success)
+            {
+                MessageBox.Show("Email correct!");
+                File.AppendAllText("myEmailadresses.txt", TBox_SendMail.Text.ToString() + "\n");
+                TBox_SendMail.Text = "Ditt mailadress...";
+            }
+            else
+            {
+                MessageBox.Show("Email inte correct!");
+                File.AppendAllText("myEmailadresses.txt", "Fel mailadress, " + DateTime.Now + "\n");
+                TBox_SendMail.Text = "Ditt mailadress...";
+            }
+
+        }
+
+        private void B_Showemailadresses_Click(object sender, RoutedEventArgs e)
+        {
+            string fil = File.ReadAllText("myEmailadresses.txt");
+
+            TBlock_ShowMaillist.Text = fil + "\n";
         }
     }
 }
